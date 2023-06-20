1e10f23d44473d85603abd757ebf6891efd62cfd
Andreu <andreubelkin@gmail.com>
2023-06-20 07:07:15 +0500

    add title page, setting default route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import { FC } from "react";
+import { Helmet } from "react-helmet";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 const App: FC = () => {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Helmet>
+        <title>Твоя.Еда - Доставка вкусных блюд</title>
+      </Helmet>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<Navigate to={"/menu"} replace />} />
+          <Route path="/menu" element="Menu" />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
