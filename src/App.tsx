f6b70e1e1fc0e6066aa094388f6cb194edad890c
Andreu <andreubelkin@gmail.com>
2023-06-20 07:15:19 +0500

    create basic html structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { Helmet } from "react-helmet";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import MenuPage from "./pages/MenuPage";
 
 const App: FC = () => {
   return (
@@ -8,12 +9,17 @@ const App: FC = () => {
       <Helmet>
         <title>Твоя.Еда - Доставка вкусных блюд</title>
       </Helmet>
-      <BrowserRouter>
-        <Routes>
-          <Route path="*" element={<Navigate to={"/menu"} replace />} />
-          <Route path="/menu" element="Menu" />
-        </Routes>
-      </BrowserRouter>
+
+      <div className="container">
+        <header>Navigation</header>
+        <BrowserRouter>
+          <Routes>
+            <Route path="*" element={<Navigate to={"/menu"} replace />} />
+            <Route path="/menu" element={<MenuPage />} />
+          </Routes>
+        </BrowserRouter>
+        <footer>Footer</footer>
+      </div>
     </div>
   );
 };
---
+++ src/pages/MenuPage.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+const MenuPage: FC = () => {
+  return (
+    <main className="menu">
+      <section className="offers">Offers</section>
+      <section className="menu-list">Menu List</section>
+      <section className="apps">Apps</section>
+    </main>
+  );
+};
+
+export default MenuPage;
