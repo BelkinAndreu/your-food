3e3d9b65e7922ba3edafb20a040d0cfe46bfc5ef
Andreu <andreubelkin@gmail.com>
2023-06-22 14:27:29 +0500

    create store & productsSlice

---
+++ src/store/features/productsSlice.ts
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../store";
+
+type ICategoryProducts = "snack" | "hotDish" | "salad" | "dessert" | "drink";
+
+export interface IProduct {
+  id: number;
+  category: ICategoryProducts;
+  name: string;
+  description: string;
+  price: number;
+  photo: string;
+}
+
+export interface IProductsState {
+  counter: number;
+  products: IProduct[];
+}
+
+const initialState: IProductsState = {
+  counter: 1,
+  products: [
+    {
+      id: 0,
+      category: "hotDish",
+      name: "Казан - кебаб",
+      description: "Горячее блюдо из мяса и картофеля.",
+      price: 399,
+      photo: "",
+    },
+  ],
+};
+
+export const productsSlice = createSlice({
+  name: "products",
+  initialState: initialState,
+  reducers: {},
+});
+
+// Action creators are generated for each case reducer function
+export const {} = productsSlice.actions;
+export const selectCount = (state: RootState) => state.counter.value;
+export default productsSlice.reducer;
